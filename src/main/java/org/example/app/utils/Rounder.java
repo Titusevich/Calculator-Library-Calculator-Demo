acb680bc3a3b3d1fec9f03c85e9ccefc1979c2e9
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-26 13:55:31 +0200

    Add new class Rounder and its method roundValue

---
+++ src/main/java/org/example/app/utils/Rounder.java
@@ -0,0 +1,11 @@
+package org.example.app.utils;
+
+import java.text.DecimalFormat;
+
+public class Rounder {
+
+    public static String roundValue(double value){
+        DecimalFormat df = new DecimalFormat("0.00");
+        return df.format(value);
+    }
+}
