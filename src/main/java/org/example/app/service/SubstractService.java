cda5cc2b5b1ee55b72132e8a5d9d43ff2e16da5b
Titusevich <nickitatitaevskiy@gmail.com>
2025-01-26 14:46:29 +0200

    Add new class SubstractService and its methods

---
+++ src/main/java/org/example/app/service/SubstractService.java
@@ -0,0 +1,18 @@
+package org.example.app.service;
+
+import org.example.app.utils.Rounder;
+
+public class SubstractService {
+
+    public String getResult(double num1, double num2) {
+        return roundResult(subtract(num1, num2));
+    }
+
+    double subtract(double num1, double num2) {
+        return num1 - num2;
+    }
+
+    String roundResult(double res) {
+        return Rounder.roundValue(res);
+    }
+}
